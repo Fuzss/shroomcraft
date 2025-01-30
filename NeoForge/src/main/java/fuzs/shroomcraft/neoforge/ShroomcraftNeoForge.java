4090ed3c050e996b90c04d9e45645387a4a68ffd
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-01-30 14:27:57 +0100

    Update ShroomcraftNeoForge.java

--- NeoForge/src/main/java/fuzs/shroomcraft/neoforge/ShroomcraftNeoForge.java
+++ NeoForge/src/main/java/fuzs/shroomcraft/neoforge/ShroomcraftNeoForge.java
@@ -12,13 +12,12 @@ import fuzs.shroomcraft.data.tags.ModBlockTagProvider;
 import fuzs.shroomcraft.data.tags.ModCluckshroomVariantTagProvider;
 import fuzs.shroomcraft.data.tags.ModEntityTypeTagProvider;
 import fuzs.shroomcraft.data.tags.ModItemTagProvider;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Shroomcraft.MOD_ID)
 public class ShroomcraftNeoForge {
 
-    public ShroomcraftNeoForge(ModContainer modContainer) {
+    public ShroomcraftNeoForge() {
         ModConstructor.construct(Shroomcraft.MOD_ID, Shroomcraft::new);
         DataProviderHelper.registerDataProviders(Shroomcraft.MOD_ID,
                 ModDatapackRegistriesProvider::new,
